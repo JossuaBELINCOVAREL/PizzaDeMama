25c638e1db8564369a5a2c944148dcd10a50966f
Joko <belinjossua@gmail.com>
2025-03-24 12:22:33 +0100

    fix admin and menu.views

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
 
-# Create your views here.
+
+def index(request):
+    pizzas = Pizza.objects.all()
+    pizzas_names_prices = [pizza.nom + " : " + str(pizza.prix) + "$" for pizza in pizzas] # récupérer le nom et le prix de chaque pizza
+    pizzas_names_prices_str = ", ".join(pizzas_names_prices)
+    return HttpResponse("Les Pizzas : " + pizzas_names_prices_str)
