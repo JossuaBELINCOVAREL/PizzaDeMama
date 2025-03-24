6e5c154b3d205ba98888ca7e8bce0d49cf51722f
Joko <belinjossua@gmail.com>
2025-03-24 11:47:35 +0100

    First app

---
+++ pizzamama/menu/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Pizza(models.Model):
+    nom = models.CharField(max_length=100)
+    ingredients = models.CharField(max_length=400)
+    prix = models.FloatField(default=0)
+    vegetarienne = models.BooleanField(default=False)
