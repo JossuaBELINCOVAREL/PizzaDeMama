d4f1824a69782cf6a64355390ded9b0bc795e097
Joko <belinjossua@gmail.com>
2025-03-24 13:43:36 +0100

    menu page

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -4,10 +4,10 @@ from .models import Pizza
 
 
 def index(request):
-    
+
     '''pizzas = Pizza.objects.all()
-    pizzas_names_prices = [pizza.nom + " : " + str(pizza.prix) + "$" for pizza in pizzas] # récupérer le nom et le prix de chaque pizza
+    pizzas_names_prices = [pizza.nom + " : " + str(pizza.prix) + "€" for pizza in pizzas] # récupérer le nom et le prix de chaque pizza
     pizzas_names_prices_str = ", ".join(pizzas_names_prices)
     return HttpResponse("Les Pizzas : " + pizzas_names_prices_str)'''
 
-    return render(request, 'menu/index.html', {'pizzas': Pizza.objects.all()})
+    return render(request, 'menu/index.html', {'pizzas': Pizza.objects.all().order_by('prix')})
