b1d18918a6c2cf8d81814988cd69f4266a183616
Joko <belinjossua@gmail.com>
2025-03-24 17:04:08 +0100

    add md

--- pizzamama/menu/urls.py
+++ pizzamama/menu/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'menu' # PErmet de différencier les urls (namespace)
+app_name = 'menu' # Permet de différencier les urls (namespace)
 
 urlpatterns = [
     path('', views.index, name='index'),
