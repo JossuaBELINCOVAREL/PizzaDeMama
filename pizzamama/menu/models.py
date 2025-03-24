77598c84a5a0ccfb0a4466a7410b82cf55a4830c
Joko <belinjossua@gmail.com>
2025-03-24 12:03:53 +0100

    structure admin

--- pizzamama/menu/models.py
+++ pizzamama/menu/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 class Pizza(models.Model):
     nom = models.CharField(max_length=100)
     ingredients = models.CharField(max_length=400)
     prix = models.FloatField(default=0)
     vegetarienne = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nom
