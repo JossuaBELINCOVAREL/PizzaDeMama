3a56424b19e949a1952f08d7843ac6a3da14ec44
Joko <belinjossua@gmail.com>
2025-03-26 14:47:01 +0100

    add API

--- pizzamama/menu/api_urls.py
+++ pizzamama/menu/api_urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'menu' # Permet de différencier les urls (namespace)
 
 urlpatterns = [
-    path('GetPizzas', views.api_get_pizzas)
+    path('GetPizzas', views.api_get_pizzas),
 ]
